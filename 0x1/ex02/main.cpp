50b5d55120a60b8bd0883005520bf7147c17058a
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2023-11-30 22:46:30 +0100

    Feat: started on 0x1/ex02

---
+++ 0x1/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(void)
+{
+	std::string str;
+	std::string* str_ptr;
+	std::string &str_ref = str;
+
+	str = "HI THIS IS BRAIN";
+	str_ptr = &str;
+
+	std::cout << std::hex << &str << std::endl;
+	std::cout << std::hex << str_ptr << std::endl;
+	std::cout << std::hex << str_ref << std::endl;
+}
