85caf64b8854e75af3f2aef1cc996d9ddf64926e
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2023-12-10 23:05:16 +0100

    Fix: Added missing files in 0x1/ex01

--- 0x1/ex01/main.cpp
+++ 0x1/ex01/main.cpp
@@ -1,18 +1,6 @@
 #include"Zombie.hpp"
 
-Zombie* zombieHorde( int N, std::string name )
-{
-	int i;
-	std::string tmp_name;
-
-	if (N <= 0)
-		return (NULL);
-	Zombie* z = new Zombie[N];
-	i = 0;
-	while (i < N)
-		z[i++].set_name(name);
-	return (z);
-}
+Zombie* zombieHorde( int N, std::string name );
 
 int main(void)
 {
