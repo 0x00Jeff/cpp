4f343da2bb5866df5a419c3fa42dfb70557cd6b8
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-03-21 16:09:25 +0000

    Feat: finished 0x6

--- 0x6/ex02/main.cpp
+++ 0x6/ex02/main.cpp
@@ -1,11 +1,13 @@
 #include"Base.hpp"
-#include<ctime>
+#include<sys/time.h>
 
 Base *generate( void )
 {
 	Base * instance = NULL;
+	struct timeval t;
+	gettimeofday(&t, NULL);
 
-	srand(time(0));
+	srand(t.tv_sec + t.tv_usec);
 	switch (rand() & 0b11)
 	{
 		case 0: std::cout << "returning A " << std::endl ; instance = new A; break;
@@ -15,8 +17,46 @@ Base *generate( void )
 	return instance;
 }
 
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		Base& placeholder = dynamic_cast<A&>(p);
+		static_cast<void>(placeholder);
+		std::cout << "A" << std::endl;
+	}
+	catch (...){}
+
+	try
+	{
+		Base& placeholder = dynamic_cast<B&>(p);
+		static_cast<void>(placeholder);
+		std::cout << "B" << std::endl;
+	}
+	catch (...){}
+
+	try
+	{
+		Base& placeholder = dynamic_cast<C&>(p);
+		static_cast<void>(placeholder);
+		std::cout << "C" << std::endl;
+	}
+	catch (...){}
+}
+
 int main()
 {
 	Base *b = generate();
+	identify(b);
 	delete b;
 }
