290500f215625f72a10510f14fdc1db4f5952f61
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-09-10 01:56:41 +0100

    Feat: finished compiling the opcode

--- 0x9/ex01/Rpn.cpp
+++ 0x9/ex01/Rpn.cpp
@@ -3,7 +3,7 @@
 // constructors
 Rpn::Rpn(string &notation)
 {
-	parseNotation(notation);
+	compileRpnNotation(validateRpnNotation(notation));
 }
 
 const char *Rpn::Error::what() const throw()
@@ -17,16 +17,155 @@ int Rpn::isNumber(char c)
 	return (c >= '0' && c <= '9');
 }
 
-// private class funcationalities
-void Rpn::parseNotation(string &notation)
+int Rpn::isValidOperand(char c)
 {
-	stringstream notationStream(notation);
-	string notationElem;
-
-	while(notationStream >> notationElem)
-		checkAndPush(notationElem);
+	return isNumber(c);
 }
 
+int Rpn::isValidOpcode(char op)
+{
+	return (op == ADD_OPERATION \
+			|| op == SUB_OPERATION\
+			|| op == MUL_OPERATION \
+			|| op == DIV_OPERATION);
+}
+
+vector<string> Rpn::splitTokensBySpace(string &blob)
+{
+	string tmpToken;
+	string tmpTmpToken;
+	vector<string> tokens;
+	stringstream ss(blob);
+
+	if (!blob.size())
+		return tokens;
+
+	while(ss >> tmpToken)
+		tokens.push_back(tmpToken);
+
+	return tokens;
+}
+
+// private class funcationalities
+vector<string> Rpn::validateRpnNotation(string &notation)
+{
+	vector<string> tokens = splitTokensBySpace(notation);
+	cout << notation << endl;
+	if (!tokens.size())
+		throw Rpn::Error();
+
+	vector<string>::iterator it = tokens.begin();
+	vector<string>::iterator ite = tokens.end();
+
+	// TODO : use this?
+	//size_t opcodesCount = 0;
+	size_t operandsCount = 0;
+	char byte;
+
+	while(it != ite)
+	{
+		if ((*it).size() != 1)
+		{			cerr << "[DEBUG] operator/operand too long <" << *it << ">" << endl;
+			throw Rpn::Error();
+		}
+
+		byte = (*it)[0];
+		cout << "byte = " << byte << endl;
+
+		if (isValidOperand(byte))
+				operandsCount++;
+		else if(isValidOpcode(byte))
+		{
+			if (operandsCount < 2)
+			{
+				cerr << "[DEBUG] not enough operands for operator" << endl;
+				throw Rpn::Error();
+			}
+			operandsCount--;
+		}
+		else
+		{
+			cerr << "[DEBUG] invalid opcode or operand <" << byte << ">" << endl;
+			throw Rpn::Error();
+		}
+
+		it++;
+	}
+
+	if (operandsCount != 1)
+	{
+		cerr << "[DEBUG] unused operands in notation" << endl;
+		throw Rpn::Error();
+	}
+
+	return tokens;
+}
+
+void Rpn::compileRpnNotation(vector<string> tokens)
+{
+	vector<string>::iterator it = tokens.begin();
+	vector<string>::iterator ite = tokens.end();
+
+	bool firstOpcode = true;
+
+	while(it != ite)
+	{
+		char byte = (*it)[0];
+
+		if (isValidOpcode(byte))
+		{
+			cerr << "[DEBUG] found valid opcode <" << byte << ">" << endl;
+			if (firstOpcode)
+			{
+				appendOpcodeToByteCode(byte, firstOpcode);
+				appendOperandToByteCode((*getNextOperand(tokens))[0]);
+				appendOperandToByteCode((*getNextOperand(tokens))[0]);
+				firstOpcode = false;
+			}
+			else
+			{
+				appendOpcodeToByteCode(byte, firstOpcode);
+				appendOperandToByteCode((*getNextOperand(tokens))[0]);
+			}
+		}
+		it++;
+	}
+}
+
+vector<string>::iterator Rpn::getNextOperand(vector<string> &opcodes)
+{
+	static vector<string>::iterator it;
+	static vector<string>::iterator ite;
+	vector<string>::iterator nextOperand = opcodes.end();
+
+	int byte;
+
+	if (it != opcodes.begin())
+		it = opcodes.begin();
+
+	if (ite != opcodes.end())
+		ite = opcodes.end();
+
+	while(it != ite)
+	{
+		byte = (*it)[0];
+		it++;
+		if (isValidOperand(byte))
+		{
+			cerr << "[DEBUG] found valid operand <" << byte << ">" << endl;
+			nextOperand = it - 1;
+			break;
+		}
+	}
+	if (nextOperand == opcodes.end())
+	{
+		cerr << "[DEBUG] if you're seeing this then there is a serious bug in your code xd" << endl;
+		exit(0);
+	}
+	return nextOperand;
+}
+
+/*
 void Rpn::checkAndPush(string &elem)
 {
 	if (elem.size() > 1)
@@ -43,11 +182,14 @@ void Rpn::checkAndPush(string &elem)
 			return;
 	}
 
-	if (!isNumber(elemChar))
+	if (!isValidOperand(elemChar))
 		throw Rpn::Error();
 	push_rsp(elemChar - '0');
 }
+*/
 
+
+/*
 int Rpn::exec_opcode(char opcode)
 {
 	int arg1 = pop_rsp();
@@ -65,26 +207,62 @@ int Rpn::exec_opcode(char opcode)
 	}
 	return (res);
 }
+*/
 
-void Rpn::push_rsp(int data)
+// push operations
+void Rpn::appendOperationToByteCode(char operation, bool firstOperation)
 {
-	s.push(data);
+	cerr << "[DEBUG] adding operation <" << operation << "> with firstOperation = " << firstOperation << endl;
+	switch(operation)
+	{
+		case ADD_OPERATION: appendOpcodeToByteCode(ADD, firstOperation); break;
+		case SUB_OPERATION: appendOpcodeToByteCode(SUB, firstOperation); break;
+		case MUL_OPERATION: appendOpcodeToByteCode(MUL, firstOperation); break;
+		case DIV_OPERATION: appendOpcodeToByteCode(DIV, firstOperation); break;
+		default:
+			cerr << "[DEBUG] problem in current operation <" << operation << ">" << endl;
+			throw Rpn::Error();
+	}
 }
+
+void Rpn::appendOperandToByteCode(char data)
+{
+	appendByteToByteCode(data);
+}
+
+void Rpn::appendOpcodeToByteCode(char opcode, bool firstOpcode)
+{
+	if (firstOpcode)
+		opcode = FIRST_OPCODE(opcode);
+	appendByteToByteCode(opcode);
+}
+
+void Rpn::appendByteToByteCode(char byte)
+{
+	s.opcodes.push_back(byte);
+}
+
 
 int Rpn::pop_rsp(void)
 {
 	int stackTop;
-	if (!s.size())
+	if (!s.stack.size())
 		throw Rpn::Error();
 
-	stackTop = s.top();
-	s.pop();
+	stackTop = s.stack.top();
+	s.stack.pop();
+	s.rip++;
 	return stackTop;
+}
+
+void Rpn::push_rsp(int data)
+{
+	s.stack.push(data);
 }
 
 int Rpn::getResult()
 {
-	if (s.size() != 1)
+	if (s.stack.size() != 1)
 		throw Rpn::Error();
 	return pop_rsp();
 }
--- 0x9/ex01/Rpn.hpp
+++ 0x9/ex01/Rpn.hpp
@@ -2,8 +2,10 @@
 #include<sstream>
 #include<iostream>
 #include<stack>
+#include<vector>
 
 using std::string;
+using std::vector;
 using std::stack;
 
 using std::ifstream;
@@ -13,6 +15,38 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+#define FIRST_OPCODE(x) (x | (1 << 4))
+#define IS_FIRST_OPCODE(x) (x >> 4)
+#define GET_OP(x) (x & 7)
+
+enum BYTECODE
+{
+	FIRST_ADD,
+	FIRST_SUB,
+	FIRST_MUL,
+	FIRST_DIV,
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+	STOP
+};
+
+enum OPERATIONS
+{
+	ADD_OPERATION = '+',
+	SUB_OPERATION = '-',
+	MUL_OPERATION = '*',
+	DIV_OPERATION = '/',
+};
+
+struct state 
+{
+	vector<char> opcodes;
+	stack<int> stack;
+	size_t rip;
+};
+
 class Rpn
 {
 	private:
@@ -20,8 +54,7 @@ class Rpn
 		Rpn(Rpn &ref);
 		Rpn &operator=(Rpn &ref);
 
-		// class attributes
-		stack<int> s;
+		state s;
 
 		// exceptions
 		class Error : public std::exception
@@ -31,12 +64,31 @@ class Rpn
 
 		// static functions
 		static int isNumber(char c);
+		static int isValidOperand(char c);
+		static int isValidOpcode(char c);
+		static vector<string> splitTokensBySpace(string &tokens);
 
 		// private class funcationalities
-		void parseNotation(string &notation);
-		void checkAndPush(string &notationElem);
-		int exec_opcode(char opcode);
+		vector<string> validateRpnNotation(string &notation);
+		void compileRpnNotation(vector<string> tokens);
 		
+
+		// bytecode operations
+		void appendToByteCode(char data);
+
+		// stack operations
+		void getNextOperand();
+		void appendByteToByteCode(char byte);
+		void appendOperationToByteCode(char operation, bool firstOperation);
+		void appendOpcodeToByteCode(char opcode, bool firstOpcode);
+		void appendOperandToByteCode(char data);
+		void pushOpcode(char data);
+		void pushOperand(char data);
+		vector<string>::iterator getNextOperand(vector<string> &opcodes);
+
+		// 
+		void pushData(char data);
+
 		void push_rsp(int data);
 		int pop_rsp(void);
 
