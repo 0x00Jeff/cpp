6b11f1b91f94759ee02dca2bf375075b7dd80dbb
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-09-16 19:08:32 +0100

    Feat: 0x9 finished

---
+++ 0x9/ex02/PmergeMe.hpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<ctime>
+#include<exception>
+#include<sstream>
+#include<vector>
+
+#include<list>
+#include<deque>
+
+using std::vector;
+using std::deque;
+using std::pair;
+
+using std::string;
+using std::stringstream;
+
+using std::make_pair;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+class PmergeMe 
+{
+	private:
+		vector<int> inputVec;
+
+		deque<int> inputDeq;
+
+		// exceptions
+		class invalidNumber : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+
+		// class functionalities
+		void parseInput(string input);
+
+		template <typename CONTAINER>
+		void binaryInsertionSort(CONTAINER &c, size_t start, size_t size);
+
+		template <typename CONTAINER>
+		void mergeSort(CONTAINER &c, size_t start1, size_t size1, size_t start2, size_t size2);
+
+		template <typename CONTAINER>
+		void recursiveSort(CONTAINER &c, size_t start, size_t size, int iter);
+
+		template <typename CONTAINER>
+		void displayContainerContent(CONTAINER &c);
+
+	public:
+		// constructor
+		PmergeMe(string input);
+
+		// exception
+		class emptyContainerPop : public std::exception
+		{
+			virtual const char *what() const throw();
+		};
+
+		// class functionalities
+		void sort(void);
+
+		// destructor
+		~PmergeMe();
+};
